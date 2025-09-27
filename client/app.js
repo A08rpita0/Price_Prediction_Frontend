ab1049afe851f9a3b10f7f15c4c9284895791cbd
A08rpita <arpitakhobragade2308@gmail.com>
2025-09-28 02:16:41 +0530

    Initial commit

--- client/app.js
+++ client/app.js
@@ -1,62 +1,133 @@
+// ----------------------
+// Utility functions
+// ----------------------
+
+// Get selected number of bathrooms
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i in uiBathrooms) {
-    if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
+  for (var i = 0; i < uiBathrooms.length; i++) {
+    if (uiBathrooms[i].checked) {
+      return parseInt(uiBathrooms[i].value);
     }
   }
-  return -1; // Invalid Value
+  return -1;
 }
 
+// Get selected BHK
 function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
-  for(var i in uiBHK) {
-    if(uiBHK[i].checked) {
-        return parseInt(i)+1;
+  for (var i = 0; i < uiBHK.length; i++) {
+    if (uiBHK[i].checked) {
+      return parseInt(uiBHK[i].value);
     }
   }
-  return -1; // Invalid Value
+  return -1;
 }
 
+// ----------------------
+// Handle price estimation
+// ----------------------
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked");
-  var sqft = document.getElementById("uiSqft");
+
+  var sqft = parseFloat(document.getElementById("uiSqft").value);
   var bhk = getBHKValue();
-  var bathrooms = getBathValue();
-  var location = document.getElementById("uiLocations");
+  var bath = getBathValue();
+  var location = document.getElementById("uiLocations").value;
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  if (!sqft || bhk <= 0 || bath <= 0 || !location) {
+    alert("Please fill all fields correctly!");
+    return;
+  }
+
+  var url = "http://127.0.0.1:5000/predict_home_price";
 
   $.post(url, {
-      total_sqft: parseFloat(sqft.value),
-      bhk: bhk,
-      bath: bathrooms,
-      location: location.value
-  },function(data, status) {
-      console.log(data.estimated_price);
-      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-      console.log(status);
+    total_sqft: sqft,
+    bhk: bhk,
+    bath: bath,
+    location: location
+  }, function (data, status) {
+    console.log(data.estimated_price);
+
+    estPrice.innerHTML = "<h2>💰 Estimated Price: " + data.estimated_price.toString() + " Lakh</h2>";
+
+    console.log(status);
+
+    // Add marker on map for predicted price
+    var coords = getLocationCoords(location);
+    if (coords) {
+      addPriceMarker(coords.lat, coords.lng, data.estimated_price.toFixed(2));
+    }
   });
 }
 
+// ----------------------
+// On page load: populate locations
+// ----------------------
 function onPageLoad() {
-  console.log( "document loaded" );
-   var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
-      if(data) {
-          var locations = data.locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
-          }
+  console.log("document loaded");
+
+  var url = "http://127.0.0.1:5000/get_location_names";
+  $.get(url, function (data, status) {
+    console.log("got response for get_location_names request");
+
+    if (data) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      $('#uiLocations').append('<option disabled selected>Select Location</option>');
+
+      for (var i in locations) {
+        var opt = new Option(locations[i], locations[i]);
+        $('#uiLocations').append(opt);
       }
+    }
   });
 }
 
 window.onload = onPageLoad;
+
+// ----------------------
+// Leaflet Map setup
+// ----------------------
+
+// Initialize Leaflet Map (Bangalore coordinates: 12.9716° N, 77.5946° E)
+var map = L.map('bangaloreMap').setView([12.9716, 77.5946], 12);
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+// Add default marker for Bangalore
+L.marker([12.9716, 77.5946])
+  .addTo(map)
+  .bindPopup('<b>Bangalore</b><br>Welcome to the Garden City!')
+  .openPopup();
+
+// ----------------------
+// Function to add markers for predicted prices
+// ----------------------
+function addPriceMarker(lat, lng, price) {
+  var marker = L.marker([lat, lng]).addTo(map);
+  marker.bindPopup(`<b>Predicted Price</b><br>₹${price} Lakh`).openPopup();
+}
+
+// ----------------------
+// Dummy coordinates for Bangalore locations
+// ----------------------
+function getLocationCoords(location) {
+  var coords = {
+    "Whitefield": {lat: 12.969, lng: 77.749},
+    "HSR Layout": {lat: 12.9121, lng: 77.6412},
+    "Indiranagar": {lat: 12.9719, lng: 77.6412},
+    "Koramangala": {lat: 12.9352, lng: 77.6245},
+    "Jayanagar": {lat: 12.925, lng: 77.5938},
+    "MG Road": {lat: 12.975, lng: 77.605},
+  };
+  return coords[location] || {lat: 12.9716, lng: 77.5946};
+}
+
+
